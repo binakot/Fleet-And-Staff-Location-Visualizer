594488afea1f504e34d9f7333aaabd3dd78bc79f
Binakot <binakot@gmail.com>
2017-05-08 15:53:43 +0300

    Add realistic map visualization and style selector.
    Also added app and author logos. And tuned up the build settings.

---
+++ FleetAndStaffLocationVisualizer/Assets/Scripts/Helpers/MapStyleSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Mapbox.Unity.MeshGeneration;
+using UnityEngine;
+
+namespace Assets.Scripts.Helpers
+{
+    [RequireComponent(typeof(MapController))]
+    public sealed class MapStyleSelector : MonoBehaviour
+    {
+        public MapStyleType MapStyle = MapStyleType.Stylized;
+        public enum MapStyleType
+        {
+            Stylized,
+            Realistic
+        }
+
+        public MapVisualization StylizedMapVisualization;
+        public MapVisualization RealisticMapVisualization;
+
+        // TODO Implement the real-time style changing. Now there is the pre-compile style changing only.
+        private void Awake()
+        {
+            var mapController = this.GetComponent<MapController>();
+            switch (MapStyle)
+            {
+                case MapStyleType.Stylized:
+                    mapController.MapVisualization = StylizedMapVisualization;
+                    mapController.Awake();
+                    break;
+
+                case MapStyleType.Realistic:
+                    mapController.MapVisualization = RealisticMapVisualization;
+                    mapController.Awake();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
